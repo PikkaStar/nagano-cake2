0244f3f4a55c477cec2cb9add40aba5cf516974b
PikkaStar <yokotaka1226@gmail.com>
2024-01-27 04:52:59 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :admin,skip: [:passwords,:registrations], controller: {
+  devise_for :admin,skip: [:passwords,:registrations], controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :users,skip: [:passwords],controller: {
+  devise_for :users,skip: [:passwords],controllers: {
     registrations: "user/registrations",
     sessions: "user/sessions"
   }
