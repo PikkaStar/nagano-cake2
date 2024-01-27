89b945880acdf3ca69883e9d9e7951d07d1790e4
PikkaStar <yokotaka1226@gmail.com>
2024-01-27 00:57:25 +0000

    before-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin,skip: [:passwords,:registrations], controller: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users,skip: [:passwords],controller: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
