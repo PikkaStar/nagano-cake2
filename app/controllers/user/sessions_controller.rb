d379ca34a44060597cb5173b702d1ac703dd751b
PikkaStar <yokotaka1226@gmail.com>
2024-02-06 11:48:34 +0000

    registration sessions

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,27 +1,20 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+   before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
+   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+   end
 end
